527450a2bbb263a83aa55ac9fbcdb18661044fab
manato kuroda <a7108100@gmail.com>
2021-11-10 16:46:27 +0900

    feat: add ent

---
+++ ent/schema/test.go
@@ -0,0 +1,32 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/schema/field"
+)
+
+// Test holds the schema definition for the Test entity.
+type Test struct {
+	ent.Schema
+}
+
+// Fields of the Test.
+func (Test) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("name").Default(""),
+		field.Int("age"),
+		field.Time("created_at").
+			Default(time.Now).
+			SchemaType(map[string]string{
+				dialect.MySQL: "datetime DEFAULT CURRENT_TIMESTAMP",
+			}),
+	}
+}
+
+// Edges of the Test.
+func (Test) Edges() []ent.Edge {
+	return nil
+}
--- main.go
+++ main.go
@@ -1,13 +1,46 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"log"
 	"net/http"
+	"project-management-demo-backend/ent"
+
+	"github.com/go-sql-driver/mysql"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	var entOptions []ent.Option
+	entOptions = append(entOptions, ent.Debug())
+
+	mc := mysql.Config{
+		User:                 "root",
+		Passwd:               "root",
+		Net:                  "tcp",
+		Addr:                 "localhost" + ":" + "3308",
+		DBName:               "project_management_demo",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	}
+	client, err := ent.Open("mysql", mc.FormatDSN(), entOptions...)
+	if !errors.Is(err, nil) {
+		log.Fatalf("failed opening mysql client: %v", err)
+	}
+	defer func(client *ent.Client) {
+		err = client.Close()
+		if err != nil {
+			log.Fatalf("failed closing mysql connection: %v", err)
+		}
+	}(client)
+
+	if err = client.Schema.Create(context.Background()); !errors.Is(err, nil) {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
 	e := echo.New()
 
 	e.Use(middleware.Logger())
