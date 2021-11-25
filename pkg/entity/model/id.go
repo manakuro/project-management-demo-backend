8eec37b6e83d54ae873834800e1106844b8dfb49
manato kuroda <a7108100@gmail.com>
2021-11-25 17:05:26 +0900

    refactor: remove unnecessary code

--- pkg/entity/model/id.go
+++ pkg/entity/model/id.go
@@ -4,8 +4,3 @@ import "project-management-demo-backend/ent/schema/pulid"
 
 // ID implements a PULID - a prefixed ULID.
 type ID = pulid.ID
-
-// Value returns pulid.ID.
-//func (i *ID) Value() pulid.ID {
-//	return *i.ID
-//}
