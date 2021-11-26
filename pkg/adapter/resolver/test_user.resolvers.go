35a1ee707ac68781c9b84e6037c3b810a31b247c
manato kuroda <a7108100@gmail.com>
2021-11-26 13:45:32 +0900

    refactor: remove unnecessary generated code

--- pkg/adapter/resolver/test_user.resolvers.go
+++ pkg/adapter/resolver/test_user.resolvers.go
@@ -9,7 +9,6 @@ import (
 	"project-management-demo-backend/ent/schema/pulid"
 	"project-management-demo-backend/graph/generated"
 	"project-management-demo-backend/pkg/adapter/handler"
-	"project-management-demo-backend/pkg/entity/model"
 	"project-management-demo-backend/pkg/util/datetime"
 )
 
@@ -57,18 +56,3 @@ func (r *testUserResolver) UpdatedAt(ctx context.Context, obj *ent.TestUser) (st
 func (r *Resolver) TestUser() generated.TestUserResolver { return &testUserResolver{r} }
 
 type testUserResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *testUserResolver) TestTodos(ctx context.Context, obj *ent.TestUser) ([]*model.TestTodo, error) {
-	ts, err := obj.TestTodos(ctx)
-	if err != nil {
-		return nil, handler.HandleError(ctx, err)
-	}
-
-	return ts, nil
-}
