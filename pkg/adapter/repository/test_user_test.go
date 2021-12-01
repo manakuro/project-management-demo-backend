4e1d765711bc883710dbe407fa40d3a3d70f0f62
manato kuroda <a7108100@gmail.com>
2021-12-01 17:04:03 +0900

    test: use bulk insert for List test

--- pkg/adapter/repository/test_user_test.go
+++ pkg/adapter/repository/test_user_test.go
@@ -66,10 +66,30 @@ func Test_testUserRepository_List(t *testing.T) {
 					t.FailNow()
 				}
 
+				users := []struct {
+					name string
+					age  int
+				}{
+					{
+						name: "test",
+						age:  10,
+					},
+					{
+						name: "test2",
+						age:  11,
+					},
+					{
+						name: "test3",
+						age:  12,
+					},
+				}
+				bulk := make([]*ent.TestUserCreate, len(users))
+				for i, u := range users {
+					bulk[i] = client.TestUser.Create().SetName(u.name).SetAge(u.age)
+				}
+
 				_, err = client.TestUser.
-					Create().
-					SetName("test").
-					SetAge(20).
+					CreateBulk(bulk...).
 					Save(ctx)
 				if err != nil {
 					t.Error(err)
@@ -81,7 +101,7 @@ func Test_testUserRepository_List(t *testing.T) {
 			},
 			assert: func(t *testing.T, got []*model.TestUser, err error) {
 				assert.Nil(t, err)
-				assert.Equal(t, 1, len(got))
+				assert.Equal(t, 3, len(got))
 			},
 			args: args{
 				ctx: context.Background(),
