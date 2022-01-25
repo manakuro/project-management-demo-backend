2920e7414246c7ecb276cbf022b536846ddabf79
manato kuroda <a7108100@gmail.com>
2022-01-26 08:39:24 +0900

    feat: add task list completed status to graphql query

--- pkg/adapter/controller/controller.go
+++ pkg/adapter/controller/controller.go
@@ -2,25 +2,26 @@ package controller
 
 // Controller holds the controllers for the entire across application
 type Controller struct {
-	Auth               interface{ Auth }
-	Color              interface{ Color }
-	FavoriteProject    interface{ FavoriteProject }
-	FavoriteWorkspace  interface{ FavoriteWorkspace }
-	Icon               interface{ Icon }
-	Me                 interface{ Me }
-	MyTasksTabStatus   interface{ MyTasksTabStatus }
-	Project            interface{ Project }
-	ProjectBaseColor   interface{ ProjectBaseColor }
-	ProjectIcon        interface{ ProjectIcon }
-	ProjectLightColor  interface{ ProjectLightColor }
-	ProjectTaskColumn  interface{ ProjectTaskColumn }
-	ProjectTeammate    interface{ ProjectTeammate }
-	TaskColumn         interface{ TaskColumn }
-	TaskSection        interface{ TaskSection }
-	Teammate           interface{ Teammate }
-	TeammateTaskColumn interface{ TeammateTaskColumn }
-	TestTodo           interface{ TestTodo }
-	TestUser           interface{ TestUser }
-	Workspace          interface{ Workspace }
-	WorkspaceTeammate  interface{ WorkspaceTeammate }
+	Auth                    interface{ Auth }
+	Color                   interface{ Color }
+	FavoriteProject         interface{ FavoriteProject }
+	FavoriteWorkspace       interface{ FavoriteWorkspace }
+	Icon                    interface{ Icon }
+	Me                      interface{ Me }
+	MyTasksTabStatus        interface{ MyTasksTabStatus }
+	Project                 interface{ Project }
+	ProjectBaseColor        interface{ ProjectBaseColor }
+	ProjectIcon             interface{ ProjectIcon }
+	ProjectLightColor       interface{ ProjectLightColor }
+	ProjectTaskColumn       interface{ ProjectTaskColumn }
+	ProjectTeammate         interface{ ProjectTeammate }
+	TaskColumn              interface{ TaskColumn }
+	TaskListCompletedStatus interface{ TaskListCompletedStatus }
+	TaskSection             interface{ TaskSection }
+	Teammate                interface{ Teammate }
+	TeammateTaskColumn      interface{ TeammateTaskColumn }
+	TestTodo                interface{ TestTodo }
+	TestUser                interface{ TestUser }
+	Workspace               interface{ Workspace }
+	WorkspaceTeammate       interface{ WorkspaceTeammate }
 }
