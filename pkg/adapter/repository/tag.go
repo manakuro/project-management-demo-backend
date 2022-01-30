cf40189909d7b3507a6b77a08c793a5ae2bb6b29
manato kuroda <a7108100@gmail.com>
2022-01-30 09:12:41 +0900

    feat: add tag to graphql query

---
+++ pkg/adapter/repository/tag.go
@@ -0,0 +1,95 @@
+package repository
+
+import (
+	"context"
+	"project-management-demo-backend/ent"
+	"project-management-demo-backend/pkg/entity/model"
+	ur "project-management-demo-backend/pkg/usecase/repository"
+	"project-management-demo-backend/pkg/util/collection"
+)
+
+type tagRepository struct {
+	client *ent.Client
+}
+
+// NewTagRepository generates tag repository
+func NewTagRepository(client *ent.Client) ur.Tag {
+	return &tagRepository{client: client}
+}
+
+func (r *tagRepository) Get(ctx context.Context, where *model.TagWhereInput) (*model.Tag, error) {
+	q := r.client.Tag.Query()
+
+	q, err := where.Filter(q)
+	if err != nil {
+		return nil, model.NewInvalidParamError(nil)
+	}
+
+	res, err := q.Only(ctx)
+
+	if err != nil {
+		if ent.IsNotSingular(err) {
+			return nil, model.NewNotFoundError(err, nil)
+		}
+		if ent.IsNotFound(err) {
+			return nil, nil
+		}
+		return nil, model.NewDBError(err)
+	}
+
+	return res, nil
+}
+
+func (r *tagRepository) List(ctx context.Context) ([]*model.Tag, error) {
+	res, err := r.client.Tag.Query().All(ctx)
+	if err != nil {
+		return nil, model.NewDBError(err)
+	}
+
+	return res, nil
+}
+
+func (r *tagRepository) ListWithPagination(ctx context.Context, after *model.Cursor, first *int, before *model.Cursor, last *int, where *model.TagWhereInput, requestedFields []string) (*model.TagConnection, error) {
+	q := r.client.Tag.Query()
+
+	if collection.Contains(requestedFields, "edges.node.color") {
+		q.WithColor()
+	}
+
+	res, err := q.Paginate(ctx, after, first, before, last, ent.WithTagFilter(where.Filter))
+	if err != nil {
+		return nil, model.NewDBError(err)
+	}
+	return res, nil
+}
+
+func (r *tagRepository) Create(ctx context.Context, input model.CreateTagInput) (*model.Tag, error) {
+	res, err := r.client.
+		Tag.
+		Create().
+		SetInput(input).
+		Save(ctx)
+
+	if err != nil {
+		return nil, model.NewDBError(err)
+	}
+
+	return res, nil
+}
+
+func (r *tagRepository) Update(ctx context.Context, input model.UpdateTagInput) (*model.Tag, error) {
+	res, err := r.client.
+		Tag.UpdateOneID(input.ID).
+		SetInput(input).
+		Save(ctx)
+
+	if err != nil {
+		if ent.IsNotFound(err) {
+			return nil, model.NewNotFoundError(err, input.ID)
+		}
+
+		return nil, model.NewDBError(err)
+	}
+
+	return res, nil
+}
