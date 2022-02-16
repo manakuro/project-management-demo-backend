3f6b4d4a786ae58a500c3d5f9497ec64bd9358d9
manato kuroda <a7108100@gmail.com>
2022-02-16 15:45:27 +0900

    chore: add workspace_id to teammate task seed

--- cmd/seed/seed/teammate_task.go
+++ cmd/seed/seed/teammate_task.go
@@ -14,72 +14,86 @@ func TeammateTask(ctx context.Context, client *ent.Client) {
 		log.Fatalf("TeammateTask failed to delete data: %v", err)
 	}
 	teammate := seedutil.GetTeammateByEmail(ctx, client, teammateFeed.manato.Email)
+	workspace := seedutil.GetWorkspace(ctx, client)
 
 	ts := []ent.CreateTeammateTaskInput{
 		{
 			TeammateID:            teammate.ID,
 			TaskID:                seedutil.GetTaskByName(ctx, client, taskAssignedFeed.task1.Name).ID,
 			TeammateTaskSectionID: seedutil.GetTeammateTaskSectionByName(ctx, client, teammateTaskSectionFeed.recentlyAssigned.Name).ID,
+			WorkspaceID:           workspace.ID,
 		},
 		{
 			TeammateID:            teammate.ID,
 			TaskID:                seedutil.GetTaskByName(ctx, client, taskAssignedFeed.task2.Name).ID,
 			TeammateTaskSectionID: seedutil.GetTeammateTaskSectionByName(ctx, client, teammateTaskSectionFeed.recentlyAssigned.Name).ID,
+			WorkspaceID:           workspace.ID,
 		},
 		{
 			TeammateID:            teammate.ID,
 			TaskID:                seedutil.GetTaskByName(ctx, client, taskAssignedFeed.task3.Name).ID,
 			TeammateTaskSectionID: seedutil.GetTeammateTaskSectionByName(ctx, client, teammateTaskSectionFeed.recentlyAssigned.Name).ID,
+			WorkspaceID:           workspace.ID,
 		},
 		{
 			TeammateID:            teammate.ID,
 			TaskID:                seedutil.GetTaskByName(ctx, client, taskAssignedFeed.task4.Name).ID,
 			TeammateTaskSectionID: seedutil.GetTeammateTaskSectionByName(ctx, client, teammateTaskSectionFeed.recentlyAssigned.Name).ID,
+			WorkspaceID:           workspace.ID,
 		},
 		{
 			TeammateID:            teammate.ID,
 			TaskID:                seedutil.GetTaskByName(ctx, client, taskAssignedFeed.task5.Name).ID,
 			TeammateTaskSectionID: seedutil.GetTeammateTaskSectionByName(ctx, client, teammateTaskSectionFeed.recentlyAssigned.Name).ID,
+			WorkspaceID:           workspace.ID,
 		},
 		{
 			TeammateID:            teammate.ID,
 			TaskID:                seedutil.GetTaskByName(ctx, client, taskAssignedFeed.task6.Name).ID,
 			TeammateTaskSectionID: seedutil.GetTeammateTaskSectionByName(ctx, client, teammateTaskSectionFeed.recentlyAssigned.Name).ID,
+			WorkspaceID:           workspace.ID,
 		},
 		{
 			TeammateID:            teammate.ID,
 			TaskID:                seedutil.GetTaskByName(ctx, client, taskAssignedFeed.task7.Name).ID,
 			TeammateTaskSectionID: seedutil.GetTeammateTaskSectionByName(ctx, client, teammateTaskSectionFeed.recentlyAssigned.Name).ID,
+			WorkspaceID:           workspace.ID,
 		},
 		{
 			TeammateID:            teammate.ID,
 			TaskID:                seedutil.GetTaskByName(ctx, client, taskAssignedFeed.task8.Name).ID,
 			TeammateTaskSectionID: seedutil.GetTeammateTaskSectionByName(ctx, client, teammateTaskSectionFeed.recentlyAssigned.Name).ID,
+			WorkspaceID:           workspace.ID,
 		},
 		{
 			TeammateID:            teammate.ID,
 			TaskID:                seedutil.GetTaskByName(ctx, client, taskAssignedFeed.task9.Name).ID,
 			TeammateTaskSectionID: seedutil.GetTeammateTaskSectionByName(ctx, client, teammateTaskSectionFeed.recentlyAssigned.Name).ID,
+			WorkspaceID:           workspace.ID,
 		},
 		{
 			TeammateID:            teammate.ID,
 			TaskID:                seedutil.GetTaskByName(ctx, client, taskAssignedFeed.task10.Name).ID,
 			TeammateTaskSectionID: seedutil.GetTeammateTaskSectionByName(ctx, client, teammateTaskSectionFeed.today.Name).ID,
+			WorkspaceID:           workspace.ID,
 		},
 		{
 			TeammateID:            teammate.ID,
 			TaskID:                seedutil.GetTaskByName(ctx, client, taskAssignedFeed.task2Subtask1.Name).ID,
 			TeammateTaskSectionID: seedutil.GetTeammateTaskSectionByName(ctx, client, teammateTaskSectionFeed.today.Name).ID,
+			WorkspaceID:           workspace.ID,
 		},
 		{
 			TeammateID:            teammate.ID,
 			TaskID:                seedutil.GetTaskByName(ctx, client, taskAssignedFeed.task2Subtask2.Name).ID,
 			TeammateTaskSectionID: seedutil.GetTeammateTaskSectionByName(ctx, client, teammateTaskSectionFeed.today.Name).ID,
+			WorkspaceID:           workspace.ID,
 		},
 		{
 			TeammateID:            teammate.ID,
 			TaskID:                seedutil.GetTaskByName(ctx, client, taskAssignedFeed.task2Subtask3.Name).ID,
 			TeammateTaskSectionID: seedutil.GetTeammateTaskSectionByName(ctx, client, teammateTaskSectionFeed.today.Name).ID,
+			WorkspaceID:           workspace.ID,
 		},
 	}
 	bulk := make([]*ent.TeammateTaskCreate, len(ts))
