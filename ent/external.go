cfc210239a783f73f6276f80322afe78341a2042
manato kuroda <a7108100@gmail.com>
2022-03-24 14:30:27 +0900

    refactor: be able to access db driver when its debug mode

--- ent/external.go
+++ ent/external.go
@@ -4,11 +4,15 @@ import (
 	"database/sql"
 	"log"
 
+	"entgo.io/ent/dialect"
 	entsql "entgo.io/ent/dialect/sql"
 )
 
 // DB provides sql.DB
 func (c *Client) DB() *sql.DB {
+	if c.debug {
+		return c.driver.(*dialect.DebugDriver).Driver.(*entsql.Driver).DB()
+	}
 	return c.driver.(*entsql.Driver).DB()
 }
 
